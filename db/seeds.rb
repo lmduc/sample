5c94f07d486c73d7f41d86fc8e774dfd2b0bc2dd
DUC LE <ducle@DUCs-MacBook-Pro-2.local>
2015-07-19 01:51:30 +0700

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+author_1 = Author.create(name: "Mark Twain", country: "America")
+Book.create(title: "Life on the Mississippi", author: author_1)
+Book.create(title: "The Adventures of Tom Sawyer", author: author_1)
+Book.create(title: "Adventures of Huckleberry Finn", author: author_1)
+
+author_2 = Author.create(name: "Stephen King", country: "America")
+Book.create(title: "The Shining", author: author_2)
+Book.create(title: "The Stand", author: author_2)
+Book.create(title: "The Dark Tower: The Gunslinger", author: author_2)
+
+author_3 = Author.create(name: "Charles Dickens", country: "England")
+Book.create(title: "Bleak House", author: author_3)
+Book.create(title: "Oliver Twist", author: author_3)
+Book.create(title: "Great Expectations", author: author_3)
