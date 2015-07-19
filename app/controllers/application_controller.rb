efef5f24c263d6fbf332b6b3ac6d283258c134de
DUC LE <ducle@DUCs-MacBook-Pro-2.local>
2015-07-19 09:36:52 +0700

    Render empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :render_empty, if: :html_request?
+
+  private
+
+  def render_empty
+    render 'layouts/empty'
+  end
+
+  def html_request?
+    request.html?
+  end
 end
